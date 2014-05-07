b3a3e72fad3c60b6d518912683a0fd5b8addbb40
Hack Reactor Students <strixcuriosus@gmail.com>
2014-05-07 16:03:12 -0700

    Set up Node server to handle GET and POST requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,50 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET') {
+    var urlPath = url.parse(req.url).path;
+    if (urlPath === '/') {
+      urlPath += 'index.html';
+    }
+    var indexPath = archive.paths.siteAssets + urlPath;
+    console.log(indexPath);
+    fs.readFile(indexPath, function(err, data){
+      if (err) {
+        res.writeHead(404, httpHelpers.headers);
+        res.end();
+        // return;
+      } else {
+        res.writeHead(200, httpHelpers.headers);
+        console.log(data.toString());
+        res.end(data.toString());
+
+      }
+      // return;
+    });
+  } else if (req.method === 'POST') {
+    var data = '';
+    req.on('data', function(chunk){
+      data += chunk;
+    });
+    req.on('end', function(){
+      var loadingPath = archive.paths.siteAssets + '/loading.html';
+      data = data.substring(4) + '\n';
+      fs.appendFile(archive.paths.list, data, function(err){
+        console.log(err);
+      });
+      res.writeHead(302, httpHelpers.headers);
+      fs.readFile(loadingPath, function(err, data){
+        res.end(data.toString());
+      });
+    });
+  } else {
+    res.writeHead(404, httpHelpers.headers)
+    res.end(archive.paths.list);
+  }
+
 };
