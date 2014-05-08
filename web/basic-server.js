8e6d24c498762512a9b3593cdf6b1f8b9c748755
Hack Reactor Students <strixcuriosus@gmail.com>
2014-05-07 22:04:40 -0700

    Begin scraping enqueued sites

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,11 @@
 var http = require("http");
 var handler = require("./request-handler");
+var fetcher = require('../workers/htmlfetcher');
 
 var port = 8000;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
 
+fetcher.fetch();
+server.listen(port, ip);
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,38 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+var fs = require('fs');
+var http = require('http-request');
+var path = require('path');
+var archive = require('../helpers/archive-helpers');
+
+//read list of urls -- expect an object of sites (passed to callback)
+//iterate over all sites in object
+  //pass each key to isArchived
+    //if not archived
+      //jsonp grab site
+      //write to archives
+
+exports.fetch = function () {
+  console.log("fetching!");
+  archive.readListOfUrls(function(err, sitesObj){
+    // console.log("readlist callback!");
+    // console.log(sitesObj);
+    for (var site in sitesObj) {
+      // console.log(site);
+      archive.isURLArchived(site, function(data) {
+        console.log("archivedcallback");
+        if (! data) {
+          var siteToFetch = 'http://' + site;
+          http.get(siteToFetch, function(err, response) {
+            response = response.buffer.toString();
+            var archivePath = archive.paths.archivedSites + '/' + site;
+            fs.writeFile(archivePath, response, function (err) {
+              if (err) { throw err; }
+            });
+          });
+        }
+      });
+    }
+  });
+
+};
